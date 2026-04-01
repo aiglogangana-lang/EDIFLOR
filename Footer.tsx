55c34791c6adeea6cc53f89ab0886a26b2d9be41
aiglogangana-lang <aiglogangana@gmail.com>
2026-04-01 07:57:37 +0100

    Remove phone number block from Footer.tsx

---
+++ Footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="contact-info">
+                <h3>Contact Us</h3>
+                <p>Email: info@ediflor.com</p>
+                <p>Address: 123 Ediflor Lane, City, Country</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
